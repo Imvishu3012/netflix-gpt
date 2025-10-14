d466ff9f4005fafd24ce84126fa514b05d300734
Vishal Prajapati <Vishalprajapati445566@gmail.com>
2025-10-15 03:46:46 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -57,7 +57,8 @@ const Header = () => {
 
           <button
             onClick={handleGptClick}
-            className="flex items-center gap-2 bg-purple-400 cursor-pointer text-white hover:bg-purple-500 transition-all duration-200 font-semibold px-4 py-2 rounded-4xl text-md  shadow-lg hover:scale-105"
+            className="flex items-center gap-2 bg-gradient-to-r text-white cursor-pointer from-purple-600 to-blue-600 hover:from-purple-700 hover:to-blue-700 transition-all duration-200 font-semibold px-4 py-2 rounded-4xl text-md  shadow-lg hover:scale-105"
+
           >
             {showGptSearch ? "Back to Browse" : "GPT Search"}
           </button>
